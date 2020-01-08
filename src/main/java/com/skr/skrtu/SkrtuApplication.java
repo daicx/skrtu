e69f3c8e0f1272353c4515731c905e280eb93eb5
dcx <807404400@qq.com>
2020-01-08 14:50:47 +0800

    1

---
+++ src/main/java/com/skr/skrtu/SkrtuApplication.java
@@ -0,0 +1,13 @@
+package com.skr.skrtu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkrtuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkrtuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skr/skrtu/SkrtuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skr.skrtu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkrtuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
