08d5bc0f5da85a914d5f4e12b3cf8fc36e8411c8
dcx <807404400@qq.com>
2020-07-19 01:04:37 +0800

    删除web

--- src/main/java/com/skr/skrtu/SkrtuApplication.java
+++ src/main/java/com/skr/skrtu/SkrtuApplication.java
@@ -1,43 +1,10 @@
 package com.skr.skrtu;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SkrtuApplication {
-
-    @Bean
-    TomcatServletWebServerFactory tomcatServletWebServerFactory() {
-        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint constraint = new SecurityConstraint();
-                constraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                constraint.addCollection(collection);
-                context.addConstraint(constraint);
-            }
-        };
-        factory.addAdditionalTomcatConnectors(createTomcatConnector());
-        return factory;
-    }
-
-    private Connector createTomcatConnector() {
-        Connector connector = new
-                Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
-        connector.setPort(80);
-        connector.setSecure(false);
-        connector.setRedirectPort(443);
-        return connector;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(SkrtuApplication.class, args);
